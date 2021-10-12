56005b1a8d89e253864275a26386ad5de42b19d2
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-10-11 21:54:42 -0400

    major refactor underway

--- main.js
+++ main.js
@@ -1,91 +1,134 @@
+/*
+KNOWN ISSUES LIST --
+--If I use "sentPacket" object in LoadMessage() to create a visual representation of the message being posted, it
+lacks an ID, so when the fetchData function runs, that message will be duplicated until the page is refreshed.
+I tried to get around this by calling fetchData at the end of loadMessage, but it doesn't appear to work or
+maybe lags long enough to be REALLY noticable.
+--If I try to create my own IDs in LoadMessage to get around the above issue, Tiny Taco Server is unable to find
+my IDs and thus those messages are stuck forever (or until server is cleaned).
+--Rework the CSS, which got extensively broken in my refactor.
+*/
+
 (function(){
     'use strict';
 
-    const userName = prompt(`What's your name?`)
-    
+    const tacoboutit = 'https://tiny-taco-server.herokuapp.com/tacoboutit/';
+    const sender = prompt('Please enter your name: ');
 
     function createElement(obj) {
-        // const obj2 = obj.sort("timestamp");
-        let name = obj.sender;
-        let id = obj.id;
-        let message = obj.text;
+        const name = obj.sender;
+        const id = obj.id;
+        const message = obj.text;
 
-        let divs = [];
-        let $divs = document.getElementsByTagName('div');
-        for (let i=0; i < $divs.length; i++){
-            divs.push(document.getElementsByTagName('div')[i].id);
-        }
         setTimeout( () => {
-        if (!(divs.includes(obj.id.toString()))) {
+            const $idChecker = document.getElementById(id);
+            if (!$idChecker) {
+                const htmlStr = 
+                `<section class="chat-bubble" id="${id}"> 
+                    <div class="profile"> 
+                        <i class="fas fa-user"></i>
+                        <h3 class="chat-name" id="${id}chn">
+                        ${name} 
+                        </h3> 
+                        <button class="edit-button" id="${id}edt"><i class="far fa-edit"></i></button> 
+                        <button class="delete-button" id="${id}del">X</button> 
+                    </div> 
+                    <p id="${id}msg">${message}</p> 
+                </section>`;        
+                
+                //Grab DOM elements
+                const $div1 = document.getElementById('div1');
+                
+                // Insert the HTML
+                $div1.insertAdjacentHTML('beforeend', htmlStr);
 
-            //Create elements
-            let $nameDiv = document.createElement('div');
-            let $delButton = document.createElement('button')
-            let $messageDiv = document.createElement('div');
-            let $editButton = document.createElement('button');
-            //Give them classes
-            $nameDiv.className += 'chat-name';
-            $delButton.className += 'delete-button';
-            $messageDiv.className += "chat-bubble";
-            $editButton.className += "edit-button";
-            //Fill the text
-            $nameDiv.innerHTML = `${name}`
-            $delButton.innerHTML = 'X';
-            $messageDiv.innerHTML = `${message}`;
-            $editButton.innerHTML = '<i class="far fa-edit"></i>';
-            //Append an ID
-            $messageDiv.id = `${id}`;
-            $delButton.id = `${id}del`;
-            $editButton.id = `${id}edt`;
+                //Grab newly created DOM elements
+                const $delButton = document.getElementById(`${id}del`);
+                const $editButton = document.getElementById(`${id}edt`);
+                
+                // Add event listeners
+                $delButton.addEventListener('click', (Event) => {
+                    deleteMessage(Event.target.id);
+                }); 
+                $editButton.addEventListener('click', (Event) => {
+                    editMessage(Event.currentTarget.id);
+                }); 
+            } //if
+        }, 100); //Timeout
+    } //Function
 
-            const currentDiv = document.getElementById("div1");
-            // Put elements on the DOM
-            document.body.insertBefore($messageDiv, currentDiv);
-            $messageDiv.appendChild($nameDiv);
-            $messageDiv.appendChild($editButton);
-            $messageDiv.appendChild($delButton);
-            
-            // Add event listeners
-            $delButton.addEventListener('click', (Event) => {
-                deleteMessage(Event.target.id);
-            }); 
-            $editButton.addEventListener('click', (Event) => {
-                editMessage(Event.currentTarget.id);
-            }); 
-        } //if
-        }, 0);
-    }
-
-// PULL LIST OF MESSAGES ON PAGE LOAD
     function fetchData() {
-        fetch('https://tiny-taco-server.herokuapp.com/tacoboutit/')
+        fetch(tacoboutit)
         .then(response => response.json())
         .then(data => {
-            // data["timestamp"].sort();
-            data.forEach(createElement)});
+            data.sort((first, second) => {
+                return first.timestamp - second.timestamp
+            })
+            data.forEach(createElement)
+            
+        });
     }
 
-    fetchData();
-    
-     const $textField = document.querySelector(".text-area");
-     const $submit = document.querySelector(".submit");
+    function deleteMessage(target){
+        const deleteKey = `${tacoboutit}${target}`.slice(0, -3);    
+        const idKey = target.slice(0, -3);
+        const $targetDiv = document.getElementById(idKey);
+        console.log(deleteKey)
+        fetch(deleteKey, {  
+            method: 'DELETE',    
+        })
+        .then (response => {
+        if (!response.ok) {
+            throw new Error('Oops! Something went wrong')
+        }
+        console.log ('Record deleted');
+        })
+        $targetDiv.remove();
+    }    
 
-     
-     $submit.addEventListener("click", loadMessage);
-    
-     function loadMessage(){
+    function editMessage(target){
+        const editKey = `${tacoboutit}${target}`.slice(0, -3);
+        let newMess = prompt('Edit your message:');
+        const idKey = editKey.slice(-3);
+        const $msgName = document.getElementById(`${idKey}chn`);
+        const $msg = document.getElementById(`${idKey}msg`);
+        $msg.textContent = newMess;
+
+        let obj = {
+            'sender': $msgName.textContent,
+            'text': newMess}
+        
+        fetch(editKey, {  
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(obj),    
+        })
+        .then (response => {
+            if (!response.ok) {
+                throw new Error('Oops! Something went wrong')
+            }
+            console.log ('Record edited');
+        });
+    }
+
+    function loadMessage(){
         let outgoing = $textField.value;
         let creationTime = Date.now();
+        // const id=`${creationTime}`.slice(-3)/1;
         let sentPacket = {
-            sender: userName,
+            sender: sender,
             text: outgoing,
             timestamp: creationTime,
+            // id: id,
         }
-         
+        // createElement(sentPacket);
+        
         fetch('https://tiny-taco-server.herokuapp.com/tacoboutit/', {
             method: 'POST',
             headers: {
-             'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
             },
             body: JSON.stringify(sentPacket),
         })
@@ -99,71 +142,14 @@
         .catch(error=> console.log('Error: ', error)) // catches errors if detected
         // .finally(() => console.log(`API's are awesome!`)); //Always fires
         $textField.value = '';
-     }
-
-    const tacoboutit = 'https://tiny-taco-server.herokuapp.com/tacoboutit/';
-    
-    function deleteMessage(target){
-         let deleteKey = tacoboutit + target;
-         let arr = Array.from(deleteKey.split(''));
-         for (let i = 0; i < 3; i++){
-             arr.pop();}
-        deleteKey = arr.join('');
-         
-        fetch(deleteKey, {  
-            method: 'DELETE',    
-        })
-        .then (response => {
-            if (!response.ok) {
-                throw new Error('Oops! Something went wrong')
-            }
-            console.log ('Record deleted');
-        })
+        fetchData();
     }
 
-    function editMessage(target){
-           console.log(target); 
-           let editKey = tacoboutit + target;
-           let arr = Array.from(editKey.split(''));
-           for (let i = 0; i < 3; i++){
-                arr.pop();}
-           editKey = arr.join('');
-           let newMess = prompt('Edit your message:');
-           let obj = {
-               'sender': userName,
-               'text': newMess}
-            
-           fetch(editKey, {  
-               method: 'PUT',
-               headers: {
-                'Content-Type': 'application/json',
-               },
-           body: JSON.stringify(obj),    
-           })
-           .then (response => {
-               if (!response.ok) {
-                   throw new Error('Oops! Something went wrong')
-               }
-               console.log ('Record edited');
-           })
-           
+    const $textField = document.querySelector(".text-area");
+    const $submit = document.querySelector(".submit");
+    $submit.addEventListener("click", loadMessage);
 
-    }
+    fetchData();
 
-
-    // fetchData(); //Manual call for testing.
-let intervalID = setInterval(fetchData, 5000);
-
-
+    let intervalID = setInterval(fetchData, 5000);
 })();
-
-        /*
-
-        WORK LIST:
-
-        Sort by created date? So need to pass in creation time, then use .sort
-        Replace with innerHTML instead of appending new stuffs?
-
-        //On-submit event listener on my submit button? Or on the form?
-
-        */
